0f32402546f45d7bbcc2b69078bd1fb4df2887ed
Pouish85 <pouish.iphone@gmail.com>
2025-07-16 09:30:50 +0200

    fix: template location

--- src/utils/readmeGenerator.ts
+++ src/utils/readmeGenerator.ts
@@ -1,4 +1,4 @@
-import * as fsp from "fs/promises";
+import * as fs from "fs/promises";
 import Handlebars from "handlebars";
 import * as path from "path";
 import { ReadmeData } from "../data";
@@ -9,14 +9,14 @@ import { ReadmeData } from "../data";
  * @returns {Promise<string>}
  */
 export async function generateReadmeContent(data: ReadmeData): Promise<string> {
-	const templatePath = path.join(__dirname, "..", "..", "templates", "basic-readme.hbs");
-
 	try {
-		const templateContent = await fsp.readFile(templatePath, { encoding: "utf-8" });
-		const template = Handlebars.compile(templateContent);
+		const templatePath = path.join(__dirname, "..", "templates", "basic-readme.hbs");
+
+		const templateFile = await fs.readFile(templatePath, { encoding: "utf-8" });
+		const template = Handlebars.compile(templateFile);
 		return template(data);
 	} catch (error) {
-		console.error("❌ Error generating README content:", error);
-		throw new Error("Failed to generate README content.");
+		console.error("❌ Erreur lors de la génération du contenu du README :", error);
+		throw new Error("Échec de la génération du contenu du README.");
 	}
 }
